148df2169f58616a29a6c26c2b8791c2d5baead2
Sithum Sandaruwan <139766672+sithum-sandaruwan@users.noreply.github.com>
2024-11-16 09:21:49 +0530

    installed Humanizer dependency ,written the simple logic

--- obj/Debug/net8.0/.Net.AssemblyInfo.cs
+++ obj/Debug/net8.0/.Net.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute(".Net")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e8c6f81a09cdf837bcccfa20c99f966c5cec757")]
 [assembly: System.Reflection.AssemblyProductAttribute(".Net")]
 [assembly: System.Reflection.AssemblyTitleAttribute(".Net")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
